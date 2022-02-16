e02c246d0092c6766f77eac3dfc6ce6f1f34b0c8
Ederson Ferreira <ederson.dev@gmail.com>
2022-02-16 20:15:52 -0300

    adicionado classes de exception

---
+++ src/main/java/com/edersondev/apitest/controller/exception/ControllerExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.edersondev.apitest.controller.exception;
+
+import java.time.LocalDateTime;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.edersondev.apitest.service.exception.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException ex, HttpServletRequest request){
+		StandardError error = new StandardError(LocalDateTime.now(),HttpStatus.NOT_FOUND.value(),ex.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+}
---
+++ src/main/java/com/edersondev/apitest/service/exception/ResourceNotFoundException.java
@@ -0,0 +1,18 @@
+package com.edersondev.apitest.service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Resource not found")
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException() {
+		super();
+	}
+	
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+}
