ce5fe801bcc0ccc9784a320ef8d2c6f53f5cc94d
Ederson Ferreira <ederson.dev@gmail.com>
2022-02-16 18:36:22 -0300

    classe user

--- src/main/java/com/edersondev/apitest/domain/User.java
+++ src/main/java/com/edersondev/apitest/domain/User.java
@@ -1,11 +1,36 @@
 package com.edersondev.apitest.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@Entity(name = "tb_user")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
 	private Integer id;
+	
 	private String name;
+	
+	@Column(unique = true)
 	private String email;
+	
 	private String password;
 	
-	
+	 
 }
