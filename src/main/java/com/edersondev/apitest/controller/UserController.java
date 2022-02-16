f50bec3f4e14c825d69fc4a29a3b9c0e166d062c
Ederson Ferreira <ederson.dev@gmail.com>
2022-02-16 20:29:47 -0300

    mudança no retorno do metodo findById com o DTO e o mapper

--- src/main/java/com/edersondev/apitest/controller/UserController.java
+++ src/main/java/com/edersondev/apitest/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.edersondev.apitest.controller;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.edersondev.apitest.domain.User;
+import com.edersondev.apitest.domain.dto.UserDTO;
 import com.edersondev.apitest.service.UserService;
 
 @RestController
@@ -15,10 +16,13 @@ import com.edersondev.apitest.service.UserService;
 public class UserController {
 
 	@Autowired
+	private ModelMapper mapper;
+	
+	@Autowired
 	private UserService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Integer id){
-		return ResponseEntity.ok().body(service.findById(id));
+	public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+		return ResponseEntity.ok().body(mapper.map(service.findById(id),UserDTO.class));
 	}
 }
