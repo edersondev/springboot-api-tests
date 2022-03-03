06ffa315ad3d2583c22518b875d2711493d0827f
Ederson Ferreira <ederson.dev@gmail.com>
2022-03-03 09:02:11 -0300

    remoção de anotação não utilizadas

--- src/main/java/com/edersondev/apitest/domain/dto/UserDTO.java
+++ src/main/java/com/edersondev/apitest/domain/dto/UserDTO.java
@@ -5,12 +5,10 @@ import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class UserDTO {
 
 	private Integer id;
